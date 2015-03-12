397adedfb35fa9bb3ca66d33f7442eefc1677a71
Danil Tulin <tulindanilcom@Danils-MacBook-Air.local>
2015-03-12 14:11:34 +0300

    fixed main.c

--- main.c
+++ main.c
@@ -2,20 +2,16 @@
 
 #include <stdio.h> //printf
 #include <string.h> //memset
-#include<stdlib.h> //exit(0);
-#include<arpa/inet.h>
-#include<sys/socket.h>
+#include <stdlib.h> //exit(0);
+#include <arpa/inet.h>
+#include <sys/socket.h>
 #include <pthread.h>
-
-#include "mysqlAPI.m"
 
 #define BUFLEN 512  //Max length of buffer
 #define PORT 8888   //The port on which to listen for incoming data
 
 int main(void)
 {
-    NSAutoreleasePool *myPool = [[NSAutoreleasePool alloc] init];
-    SQL *Database = [[SQL alloc] init];
     
     struct sockaddr_in si_me, si_other;
     
@@ -35,35 +31,8 @@ int main(void)
     {
         //try to receive some data, this is a blocking call
         recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen);
-        NSLog(@"%s",buf);
         
-        if (!strncmp(buf, "STATUS",6))
-        {
-            Hardware HardwareSet;
-            char Message[256]; Message[0] = '\0';
-            strcat(Message, [[NSString stringWithFormat:@"Temp#%d:%f EndTemp", 1, [Database GetCurrentTemp]] cStringUsingEncoding:[NSString defaultCStringEncoding]]);
-            for (int i = 1; i<=4; i++) {
-                HardwareSet = [Database StatusHardware:i];
-                strcat(Message, [[NSString stringWithFormat:@"Toggle#%d:%d%d%d",HardwareSet.ToggleNumber, HardwareSet.KeepTempSwitcher, HardwareSet.KeepTemp, HardwareSet.ToggleSwitcher] cStringUsingEncoding:[NSString defaultCStringEncoding]]);
-            }
-            strcat(Message, "End");
-            sendto(s, Message, sizeof(Message), 0, (struct sockaddr*) &si_other, slen);
-        }
-        
-        if (!strncmp(buf, "Toggle #", 8))
-        {
-            Hardware HardwareSet;
-            NSString *content = [NSString stringWithFormat:@"%s",buf];
-            content = [content substringFromIndex:8];
-            content = [content substringToIndex:[content rangeOfString:[NSString stringWithFormat:@"End"]].location];
-            HardwareSet.ToggleNumber = [content characterAtIndex:0] - '0';
-            HardwareSet.KeepTempSwitcher = [content characterAtIndex:1] - '0';
-            HardwareSet.ToggleSwitcher = [content characterAtIndex:[content length]-1] - '0';
-            content = [content substringFromIndex:2];
-            content = [content substringToIndex:[content length]-1];
-            HardwareSet.KeepTemp = [content intValue];
-            [Database UpdateHardware:HardwareSet];
-        }
+        printf("%s\n", buf);
         
         //now reply the client with the same data
         //sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)

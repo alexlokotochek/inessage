6287539db27e08b1e6f4a135b78b8e8429f63994
lokotochek <lokotochek@gmail.com>
2015-05-14 14:50:50 +0300

    resending in msg_rcv.c

--- msg_rcv.c
+++ msg_rcv.c
@@ -2,6 +2,7 @@
 #include "string.h"
 
 #include "msg_sender.h"
+#include "local.h"
 
 #include <stdlib.h>
 #include <unistd.h>
@@ -16,7 +17,40 @@ void didRecieveMessage(char *json, char **neighbours)
         return;
     }
     
-    printf("\n");
-    printMessage(message);
+    int numberOfNeighbours = 0;
+    while (neighbours[numberOfNeighbours] != NULL)
+    {
+        ++numberOfNeighbours;
+    }
+    
+    char* myIP = getMyIPV4Adress();
+    if (strcmp(myIP, message->reciever) != 0)
+    {
+        
+        
+        //debug - выводим, что мы являемся звеном пересылки
+        printf("---resending:");
+        printMessage(message);
+        printf("---end of resending\n");
+        //debug end
+        
+        
+        // пересылаем всем
+        for (int i = 0; i < numberOfNeighbours; ++i)
+        {
+            if (strcmp(neighbours[i], message->last_sender) != 0)
+            {
+                // кроме того, от кого оно пришло
+                sendMessage(message, neighbours[i]);
+            }
+        }
+    }else
+    {
+//         сообщение пришло нам
+        printf("You have a message:\n");
+        printMessage(message);
+    }
+
+    
     releaseMessage(message);
 }

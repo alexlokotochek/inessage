827dab58d796876a147ff2bbdb051b790d04a708
lokotochek <lokotochek@gmail.com>
2015-05-14 15:45:01 +0300

    tmp

--- msg_sender.c
+++ msg_sender.c
@@ -18,7 +18,8 @@ void sendMessage(Message *message, char *adress)
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     struct sockaddr_in servaddr;
     
-    message->last_sender = getMyIPV4Adress();
+    message->sender = getMyIPV4Adress();
+    
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = inet_addr(message->reciever);
